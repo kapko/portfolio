40258e691cd8fd62ea4fef9cb62e9c94eecb7d23
Kapko <kapko19@mail.ru>
2017-04-26 11:53:58 +0700

    index.js modify mongoose

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let name = require('./backend/models/list.js'),
-	app = require('./backend/module.js'),
-	LIST = require('./backend/models/list.js');
+let express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	ejs = require('ejs');
 
+app
+	.use(bodyParser.urlencoded())
+	.use(bodyParser.json())
+	.use(express.static(__dirname + '/public'))
+	.set('views', __dirname + '/public')
+	.engine('html', ejs.renderFile)
+	.set('view engine', 'html');
 
 app.get('/', (req, res) => {
 	res.render('./index.html');
